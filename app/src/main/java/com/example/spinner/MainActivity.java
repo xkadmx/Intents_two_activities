32d5a9815f88a93418346bd88ee3191c07dfbacb
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-14 20:26:40 +0200

    in public method onSendMessage: creating an intent putting extra message and sending it to another activity (ReceiveMessage)

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.spinner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.hello_darkness_message);
+        String messageText = (String) messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        Intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/spinner/ReceiveMessageActivity.java
+++ app/src/main/java/com/example/spinner/ReceiveMessageActivity.java
@@ -2,12 +2,19 @@ package com.example.spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.hello_darkness_message);
+        messageView.setText(messageText);
     }
 }
